e90a8c6c3ec8967ac061782824e22b2db6e4b2f0
@MrDenli <89518020310@mail.ru>
2023-12-07 23:45:14 +0300

    push application in dir apps

--- Django/dbb_project/dbb_project/settings.py
+++ Django/dbb_project/dbb_project/settings.py
@@ -11,10 +11,13 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os, sys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, os.path.join(PROJECT_ROOT, 'aps'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
