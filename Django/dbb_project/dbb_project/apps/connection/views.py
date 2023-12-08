34f7fde1fe5152c719063c85e4e017a756e1a5dc
@MrDenli <89518020310@mail.ru>
2023-12-08 19:37:30 +0300

    front conect to back without img

--- Django/dbb_project/dbb_project/apps/connection/views.py
+++ Django/dbb_project/dbb_project/apps/connection/views.py
@@ -1,4 +1,5 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h4>Проверка работы</h4>")
+    return render(request, 'connection/index.html')
--- Django/dbb_project/dbb_project/settings.py
+++ Django/dbb_project/dbb_project/settings.py
@@ -57,7 +57,9 @@ ROOT_URLCONF = 'dbb_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(PROJECT_ROOT, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,6 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+  os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
