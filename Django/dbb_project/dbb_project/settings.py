852cb02ba6d0c14b5745d28c49e6fd0ef344e8a1
@MrDenli <89518020310@mail.ru>
2023-12-08 18:36:13 +0300

    add html file in dir templates, add css file in dir static

--- Django/dbb_project/dbb_project/settings.py
+++ Django/dbb_project/dbb_project/settings.py
@@ -16,8 +16,8 @@ import os, sys
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.insert(0, os.path.join(PROJECT_ROOT, 'aps'))
+PROJECT_ROOT = os.path.dirname(__file__)
+sys.path.insert(0, os.path.join(PROJECT_ROOT, 'apps'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
